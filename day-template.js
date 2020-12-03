768e2fdd003183d712227bf6718fe111abf7e4ce
Mikko Reinikainen <mikko.reinikainen@reaktor.com>
2020-12-03 07:41:31 +0200

    Add JavaScript template for daily solution

---
+++ day-template.js
@@ -0,0 +1,22 @@
+#!/usr/bin/env node
+const aoc = require('./aoc')
+
+
+
+aoc.getResult1 = (lines) => {
+  let result = 0
+  lines.forEach((line) => {
+    // TODO
+  })
+  return result
+}
+
+aoc.getResult2 = (lines) => {
+  let result = 0
+  lines.forEach((line) => {
+    // TODO
+  })
+  return result
+}
+
+aoc.run()
