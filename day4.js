5fa4fe183355b3bd837561c1b3c846d83e64edd7
Mikko Reinikainen <mikko.reinikainen@reaktor.com>
2020-12-04 07:57:18 +0200

    Clean up solution for day 4

--- day4.js
+++ day4.js
@@ -1,26 +1,15 @@
 #!/usr/bin/env node
 const aoc = require('./aoc')
 
-const expectedFields = [
-'byr',
-'iyr'  ,
-'eyr'  ,
-'hgt'  ,
-'hcl'  ,
-'ecl'  ,
-'pid'
-//'cid'
-]
+const REQUIRED_FIELDS = ['byr', 'iyr', 'eyr', 'hgt', 'hcl', 'ecl', 'pid']
+const VALID_EYE_COLORS = ['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth']
 
-function getAllFound(expectedFields, foundFields ) {
-  let allFound = true
-  expectedFields.forEach(expected => {
-    if (!foundFields[expected]) {
-      console.log('field', expected, 'not found => INVALID')
-      allFound = false
-    }
-  })
-  return allFound
+function hasAllKeys(array, expectedKeys) {
+  return expectedKeys.every((expectedKey) => array[expectedKey])
+}
+
+function incrementIfTrue(counter, condition) {
+  return condition ? counter + 1 : counter
 }
 
 aoc.getResult1 = (lines) => {
@@ -29,97 +18,72 @@ aoc.getResult1 = (lines) => {
   let foundFields = []
   lines.forEach((line) => {
     if (line === '') {
-      console.log('validating', foundFields)
-      const allFound = getAllFound(expectedFields, foundFields)
-      if (allFound) {
-        console.log('VALID')
-        valid++
-      }
+      valid = incrementIfTrue(valid, hasAllKeys(foundFields, REQUIRED_FIELDS))
       foundFields = []
     } else {
       const fields = line.split(' ')
-      fields.forEach(field => {
-        const [fieldName, _] = field.split(':')
-        //console.log('fieldName', fieldName)
+      fields.forEach((field) => {
+        const [fieldName] = field.split(':')
         foundFields[fieldName] = true
       })
     }
   })
-  const allFound = getAllFound(expectedFields, foundFields)
-  if (allFound) {
-    console.log('LAST VALID')
-    valid++
-  }
+  valid = incrementIfTrue(valid, hasAllKeys(foundFields, REQUIRED_FIELDS))
   return valid
+}
+
+function hasIntegerRange(value, min, max) {
+  const integer = parseInt(value, 10)
+  return !isNaN(integer) && min <= integer && integer <= max
+}
+
+function isValidFieldValue(fieldName, fieldValue) {
+  switch (fieldName) {
+    case 'byr':
+      return hasIntegerRange(fieldValue, 1920, 2002)
+    case 'iyr':
+      return hasIntegerRange(fieldValue, 2010, 2020)
+    case 'eyr':
+      return hasIntegerRange(fieldValue, 2020, 2030)
+    case 'hgt':
+      if (fieldValue.match(/\d+cm/)) {
+        const [h] = fieldValue.split('cm')
+        return hasIntegerRange(h, 150, 193)
+      } else if (fieldValue.match(/\d+in/)) {
+        const [h] = fieldValue.split('in')
+        return hasIntegerRange(h, 59, 76)
+      }
+      break
+    case 'hcl':
+      return !!fieldValue.match(/^#[0-9a-f]{6}$/)
+    case 'ecl':
+      return VALID_EYE_COLORS.includes(fieldValue)
+    case 'pid':
+      return fieldValue.match(/^[0-9]{9}$/)
+  }
+  return false
 }
 
 aoc.getResult2 = (lines) => {
   let valid = 0
 
-  let foundFields = []
+  let validFields = []
   lines.forEach((line) => {
     if (line === '') {
-      console.log('validating', foundFields)
-      const allFound = getAllFound(expectedFields, foundFields)
-      if (allFound) {
-        console.log('VALID')
-        valid++
-      }
-      foundFields = []
+      valid = incrementIfTrue(valid, hasAllKeys(validFields, REQUIRED_FIELDS))
+      validFields = []
     } else {
       const fields = line.split(' ')
-      fields.forEach(field => {
+      fields.forEach((field) => {
         const [fieldName, fieldValue] = field.split(':')
-        let isValid = false
-        switch (fieldName) {
-          case 'byr':
-            const num = parseInt(fieldValue, 10)
-            isValid = 1920 <= num && num <= 2002
-            break;
-          case 'iyr':
-            const num2 = parseInt(fieldValue, 10)
-            isValid = 2010 <= num2 && num2 <= 2020
-            break;
-          case 'eyr':
-            const num3 = parseInt(fieldValue, 10)
-            isValid = 2020 <= num3 && num3 <= 2030
-            break
-          case 'hgt':
-            if (fieldValue.match(/\d+cm/)) {
-              const [h] = fieldValue.split('cm')
-              isValid = 150 <= h && h <= 193
-            } else if (fieldValue.match(/\d+in/)) {
-              const [h] = fieldValue.split('in')
-              isValid = 59 <= h && h <= 76
-            }
-            break;
-          case 'hcl':
-            isValid = !!fieldValue.match(/^#[0-9a-f][0-9a-f][0-9a-f][0-9a-f][0-9a-f][0-9a-f]$/)
-            break;
-          case 'ecl':
-            isValid = fieldValue === 'amb' ||
-              fieldValue === 'blu' ||
-              fieldValue === 'brn' ||
-              fieldValue === 'gry' ||
-              fieldValue === 'grn' ||
-              fieldValue === 'hzl' ||
-              fieldValue === 'oth'
-            break;
-          case 'pid':
-            isValid = fieldValue.match(/^[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]$/)
-            break;
-        }
+        const isValid = isValidFieldValue(fieldName, fieldValue)
         if (isValid) {
-          foundFields[fieldName] = true
+          validFields[fieldName] = true
         }
       })
     }
   })
-  const allFound = getAllFound(expectedFields, foundFields)
-  if (allFound) {
-    console.log('LAST VALID')
-    valid++
-  }
+  valid = incrementIfTrue(valid, hasAllKeys(validFields, REQUIRED_FIELDS))
   return valid
 }
 
