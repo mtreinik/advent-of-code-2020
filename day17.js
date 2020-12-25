d79ebf4b08d02fa0c89416beba5f684a90648b6d
Mikko Reinikainen <mikko.reinikainen@reaktor.com>
2020-12-25 21:42:00 +0200

    Refactor to reduce duplicate code

--- day17.js
+++ day17.js
@@ -3,70 +3,30 @@ const aoc = require('./aoc')
 
 aoc.parseLine = (line) => line.split('').map((char) => (char === '#' ? 1 : 0))
 
-function getMinAndMaxKey(object) {
+function getMinAndMax(object, level = 0) {
   let min = Number.MAX_SAFE_INTEGER
   let max = Number.MIN_SAFE_INTEGER
   if (!object) {
     return [min, max]
   }
-  for (const key of Object.keys(object)) {
-    min = Math.min(min, key)
-    max = Math.max(max, key)
-  }
-  return [min, max]
-}
-
-function getMinAndMaxY(object) {
-  let min = Number.MAX_SAFE_INTEGER
-  let max = Number.MIN_SAFE_INTEGER
-  if (!object) {
-    return [min, max]
-  }
-  for (const z of Object.keys(object)) {
-    const [localMin, localMax] = getMinAndMaxKey(object[z])
-    min = Math.min(min, localMin)
-    max = Math.max(max, localMax)
-  }
-  return [min, max]
-}
-
-function getMinAndMaxX(object) {
-  let min = Number.MAX_SAFE_INTEGER
-  let max = Number.MIN_SAFE_INTEGER
-  if (!object) {
-    return [min, max]
-  }
-  for (const z of Object.keys(object)) {
-    for (const y of Object.keys(object[z])) {
-      const [localMin, localMax] = getMinAndMaxKey(object[z][y])
-      min = Math.min(min, localMin)
-      max = Math.max(max, localMax)
+  if (level === 0) {
+    for (const key of Object.keys(object)) {
+      min = Math.min(min, key)
+      max = Math.max(max, key)
+    }
+  } else {
+    for (const key of Object.keys(object)) {
+      const localMinAndMax = getMinAndMax(object[key], level - 1)
+      min = Math.min(min, localMinAndMax[0])
+      max = Math.max(max, localMinAndMax[1])
     }
   }
   return [min, max]
 }
 
-function getMinAndMax4D(object) {
-  let min = Number.MAX_SAFE_INTEGER
-  let max = Number.MIN_SAFE_INTEGER
-  if (!object) {
-    return [min, max]
-  }
-  for (const z of Object.keys(object)) {
-    for (const y of Object.keys(object[z])) {
-      for (const x of Object.keys(object[z][y])) {
-        const [localMin, localMax] = getMinAndMaxKey(object[z][y][x])
-        min = Math.min(min, localMin)
-        max = Math.max(max, localMax)
-      }
-    }
-  }
-  return [min, max]
-}
-
-function show3DCubes(cubes) {
+function show3DCubes(cubes, message = '') {
   for (const z of Object.keys(cubes)) {
-    console.log('z', z)
+    console.log('z', z, message)
     for (const y of Object.keys(cubes[z])) {
       let row = ''
       for (const x of Object.keys(cubes[z][y])) {
@@ -79,15 +39,15 @@ function show3DCubes(cubes) {
 }
 
 function get3DCube(cubes, x, y, z) {
-  const [minZ, maxZ] = getMinAndMaxKey(cubes)
+  const [minZ, maxZ] = getMinAndMax(cubes)
   if (z < minZ || z > maxZ) {
     return 0
   }
-  const [minY, maxY] = getMinAndMaxKey(cubes[z])
+  const [minY, maxY] = getMinAndMax(cubes[z])
   if (y < minY || y > maxY) {
     return 0
   }
-  const [minX, maxX] = getMinAndMaxKey(cubes[z][y])
+  const [minX, maxX] = getMinAndMax(cubes[z][y])
   if (x < minX || x > maxX) {
     return 0
   }
@@ -95,11 +55,11 @@ function get3DCube(cubes, x, y, z) {
 }
 
 function set3DCube(cubes, x, y, z, value) {
-  const [minZ, maxZ] = getMinAndMaxKey(cubes)
+  const [minZ, maxZ] = getMinAndMax(cubes)
   if (z < minZ || z > maxZ) {
     cubes[z] = {}
   }
-  const [minY, maxY] = getMinAndMaxKey(cubes[z])
+  const [minY, maxY] = getMinAndMax(cubes[z])
   if (y < minY || y > maxY) {
     cubes[z][y] = {}
   }
@@ -109,12 +69,12 @@ function set3DCube(cubes, x, y, z, value) {
 }
 
 function move3DCubes(cubes) {
-  const [minZ, maxZ] = getMinAndMaxKey(cubes)
+  const [minZ, maxZ] = getMinAndMax(cubes)
   let newCubes = {}
   for (let z = minZ - 1; z <= maxZ + 1; z++) {
-    const [minY, maxY] = getMinAndMaxY(cubes)
+    const [minY, maxY] = getMinAndMax(cubes, 1)
     for (let y = minY - 1; y <= maxY + 1; y++) {
-      const [minX, maxX] = getMinAndMaxX(cubes)
+      const [minX, maxX] = getMinAndMax(cubes, 2)
       for (let x = minX - 1; x <= maxX + 1; x++) {
         let neighborCount = 0
         for (let dz = -1; dz <= 1; dz++) {
@@ -178,66 +138,36 @@ aoc.getResult1 = (lines) => {
 
 function show4DCubes(cubes) {
   for (const w of Object.keys(cubes)) {
-    for (const z of Object.keys(cubes[w])) {
-      console.log('z', z, 'w', w)
-      for (const y of Object.keys(cubes[w][z])) {
-        let row = ''
-        for (const x of Object.keys(cubes[w][z][y])) {
-          row += cubes[w][z][y][x] ? '#' : '.'
-        }
-        console.log(row)
-      }
-      console.log()
-    }
+    show3DCubes(cubes[w], 'w=' + w)
   }
 }
 
 function get4DCube(cubes, x, y, z, w) {
-  const [minW, maxW] = getMinAndMaxKey(cubes)
+  const [minW, maxW] = getMinAndMax(cubes)
   if (w < minW || w > maxW) {
     return 0
   }
-  const [minZ, maxZ] = getMinAndMaxKey(cubes[w])
-  if (z < minZ || z > maxZ) {
-    return 0
-  }
-  const [minY, maxY] = getMinAndMaxKey(cubes[w][z])
-  if (y < minY || y > maxY) {
-    return 0
-  }
-  const [minX, maxX] = getMinAndMaxKey(cubes[w][z][y])
-  if (x < minX || x > maxX) {
-    return 0
-  }
-  return cubes[w][z][y][x]
+  return get3DCube(cubes[w], x, y, z)
 }
 
 function set4DCube(cubes, x, y, z, w, value) {
-  const [minW, maxW] = getMinAndMaxKey(cubes)
+  const [minW, maxW] = getMinAndMax(cubes)
   if (w < minW || w > maxW) {
     cubes[w] = {}
   }
-  const [minZ, maxZ] = getMinAndMaxKey(cubes[w])
-  if (z < minZ || z > maxZ) {
-    cubes[w][z] = {}
-  }
-  const [minY, maxY] = getMinAndMaxKey(cubes[w][z])
-  if (y < minY || y > maxY) {
-    cubes[w][z][y] = {}
-  }
-  cubes[w][z][y][x] = value
+  cubes[w] = set3DCube(cubes[w], x, y, z, value)
   return cubes
 }
 
 function move4DCubes(cubes) {
-  const [minW, maxW] = getMinAndMaxKey(cubes)
+  const [minW, maxW] = getMinAndMax(cubes)
   let newCubes = {}
   for (let w = minW - 1; w <= maxW + 1; w++) {
-    const [minZ, maxZ] = getMinAndMaxY(cubes)
+    const [minZ, maxZ] = getMinAndMax(cubes, 1)
     for (let z = minZ - 1; z <= maxZ + 1; z++) {
-      const [minY, maxY] = getMinAndMaxX(cubes)
+      const [minY, maxY] = getMinAndMax(cubes, 2)
       for (let y = minY - 1; y <= maxY + 1; y++) {
-        const [minX, maxX] = getMinAndMax4D(cubes)
+        const [minX, maxX] = getMinAndMax(cubes, 3)
         for (let x = minX - 1; x <= maxX + 1; x++) {
           let neighborCount = 0
           for (let dw = -1; dw <= 1; dw++) {
@@ -250,7 +180,13 @@ function move4DCubes(cubes) {
                     Math.abs(dz) === 1 ||
                     Math.abs(dw) === 1
                   ) {
-                    neighborCount += get4DCube(cubes, x + dx, y + dy, z + dz, w + dw)
+                    neighborCount += get4DCube(
+                      cubes,
+                      x + dx,
+                      y + dy,
+                      z + dz,
+                      w + dw
+                    )
                   }
                 }
               }
@@ -276,13 +212,7 @@ function move4DCubes(cubes) {
 function count4DCubes(cubes) {
   let counter = 0
   for (const w of Object.keys(cubes)) {
-    for (const z of Object.keys(cubes[w])) {
-      for (const y of Object.keys(cubes[w][z])) {
-        for (const x of Object.keys(cubes[w][z][y])) {
-          counter += cubes[w][z][y][x] ? 1 : 0
-        }
-      }
-    }
+    counter += count3DCubes(cubes[w])
   }
   return counter
 }
@@ -298,7 +228,7 @@ aoc.getResult2 = (lines) => {
   show4DCubes(cubes)
   for (let i = 0; i < 6; i++) {
     cubes = move4DCubes(cubes)
-    show4DCubes(cubes)
+    // show4DCubes(cubes)
   }
   const counter = count4DCubes(cubes)
   return counter
